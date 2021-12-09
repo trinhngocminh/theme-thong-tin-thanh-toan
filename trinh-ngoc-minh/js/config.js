7c9b537b86cd20d970f64a5c134ae13cae504f04
trinhngocminh <95848209+trinhngocminh@users.noreply.github.com>
2021-12-09 15:17:02 +0700

    Add files via upload

---
+++ trinh-ngoc-minh/js/config.js
@@ -0,0 +1,46 @@
+const USER = {
+    banks: [{
+            name: "MB BANK",
+            logo: "./images/mbbank.png",
+            number: "0334955115",
+            owner: "TRAN DUC LUONG"
+        },
+ //       {
+ //           name: "BIDV BANK",
+ //           logo: "./images/bidv.png",
+ //           number: "42710000636335",
+ //           owner: "LE ANH DUC"
+ //     },
+ //       {
+ //           name: "AGRI BANK",
+ //           logo: "./images/agribank.png",
+ //           number: "xxxxxxxxxxxx",
+ //           owner: "LE ANH DUC"
+ //       },
+        {
+            name: "PAYPAL",
+            logo: "./images/paypal.png",
+            number: "ngocminhvn",
+            owner: "TRINH NGOC MINH"
+        },
+        {
+            name: "MOMO",
+            logo: "./images/momo.png",
+            number: "0387911646",
+            owner: "TRAN DUC LUONG",
+            fastTransfer: true
+        },
+        {
+            name: "ZALO PAY",
+            logo: "./images/zalopay.png",
+            number: "0334955115",
+            owner: "TRINH NGOC MINH"
+        },
+//        {
+//            name: "VIETTEL PAY",
+//            logo: "./images/viettelpay.png",
+//            number: "0386897757",
+//            owner: "LE ANH DUC"
+//        },
+    ]
+};
