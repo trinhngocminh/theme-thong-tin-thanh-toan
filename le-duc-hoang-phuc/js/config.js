bbc52ba83fdf069509f41fcc6e83efb3be8e9796
trinhngocminh <95848209+trinhngocminh@users.noreply.github.com>
2021-12-09 16:02:36 +0700

    Update config.js

--- le-duc-hoang-phuc/js/config.js
+++ le-duc-hoang-phuc/js/config.js
@@ -5,24 +5,6 @@ const USER = {
             number: "0334955115",
             owner: "LE DUC HOANG PHUC"
         },
- //       {
- //           name: "BIDV BANK",
- //           logo: "./images/bidv.png",
- //           number: "42710000636335",
- //           owner: "LE ANH DUC"
- //     },
- //       {
- //           name: "AGRI BANK",
- //           logo: "./images/agribank.png",
- //           number: "xxxxxxxxxxxx",
- //           owner: "LE ANH DUC"
- //       },
- //       {
- //           name: "PAYPAL",
- //           logo: "./images/paypal.png",
- //           number: "ngocminhvn",
- //           owner: "TRINH NGOC MINH"
- //       },
         {
             name: "MOMO",
             logo: "./images/momo.png",
@@ -30,17 +12,5 @@ const USER = {
             owner: "LE DUC HOANG PHUC",
             fastTransfer: true
         },
-//        {
-//            name: "ZALO PAY",
-//            logo: "./images/zalopay.png",
-//            number: "0334955115",
-//            owner: "TRINH NGOC MINH"
-//        },
-//        {
-//            name: "VIETTEL PAY",
-//            logo: "./images/viettelpay.png",
-//            number: "0386897757",
-//            owner: "LE ANH DUC"
-//        },
     ]
 };
